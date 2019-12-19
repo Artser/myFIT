d18de44f3773cce64b20186afab34800966cb294
Artser <artserlisa@mail.ru>
2019-12-19 08:31:55 +0300

    добавляет отзывы

--- source/js/main.js
+++ source/js/main.js
@@ -17,14 +17,14 @@ var swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 0,
   loop: true,
-  //loopFillGroupWithBlank: true,
+  loopFillGroupWithBlank: true,
 
 
   breakpoints: {
     768: {
       slidesPerView: 2,
-      spaceBetween: 30,
-      slidesPerGroup: 2
+      spaceBetween: 28,
+      slidesPerGroup: 4
     },
     1200: {
       slidesPerView: 4,
@@ -34,7 +34,45 @@ var swiper = new Swiper(".swiper-container", {
   },
 
   navigation: {
-    nextEl: ".slider__btn--next",
-    prevEl: ".slider__btn--prev"
+    nextEl: ".slider__btn--prev",
+    prevEl: ".slider__btn--next"
   }
 });
+
+var tabElement = document.querySelector(".tickets ");
+if (tabElement) {
+  var tabLinkElements = tabElement.querySelectorAll(".tickets__btn");
+  var tabElements = tabElement.querySelectorAll(".tickets__tab");
+
+  var CSS_CLASS_OPEN = "tickets__tab--open";
+  var CSS_CLASS_ACTIVE = "tickets__btn--active";
+
+  /**
+   * tabLinkElements.forEach((linkElement, index) => {
+   *
+   * }
+   */
+  tabLinkElements.forEach(function(linkElement, index) {
+    linkElement.addEventListener("click", function(evt) {
+      evt.preventDefault();
+
+      tabElements.forEach(function(currentTabElement) {
+        currentTabElement.classList.remove(CSS_CLASS_OPEN);
+      });
+      tabElements[index].classList.add(CSS_CLASS_OPEN);
+
+      tabLinkElements.forEach(function(currentLinkElement) {
+        currentLinkElement.classList.remove(CSS_CLASS_ACTIVE);
+      });
+      linkElement.classList.add(CSS_CLASS_ACTIVE);
+    });
+  });
+}
+
+var swiper2 = new Swiper('.review__list', {
+  navigation: {
+    nextEl: '.review__list-slider-next',
+    prevEl: '.review__list-slider-prev',
+  },
+});
+
